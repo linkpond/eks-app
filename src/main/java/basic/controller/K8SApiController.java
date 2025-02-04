fc0257df6a9aa9ce4ffa07b6b189a423d863306c
linkpond <linkpond0629@gmail.com>
2025-02-04 10:22:44 +0900

    update

--- src/main/java/basic/controller/K8SApiController.java
+++ src/main/java/basic/controller/K8SApiController.java
@@ -3,11 +3,15 @@ package basic.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class K8SApiController {
 	
 	@GetMapping(value = "/api/status")
 	public String k8sApiTest() {
+		log.info("k8sApiTest success");
 		return "Status - k8s-app";
 	}
 
@@ -17,6 +21,6 @@ public class K8SApiController {
 	}
 	@GetMapping(value = "/api/status02")
 	public String eksApiTestPrivate() {
-		return "Status - eks-app-private-test";
+		return "Status - eks-app-private-test-2025/02/04";
 	}
 }
